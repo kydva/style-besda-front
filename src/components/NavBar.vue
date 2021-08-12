cd8767ca0c1c268b78b3f6b9ff69296563e750dc
kydva <kydvayv@gmail.com>
2021-08-12 23:31:35 +0300

    Display logged user name and avatar on navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,42 +1,81 @@
 <template>
   <div>
 
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3 mb-4">
-    <router-link to="/" class="navbar-brand">Style Besda</router-link>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3 mb-4">
+      <router-link to="/" class="navbar-brand">Style Besda</router-link>
 
-    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/login">Sign in</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/registration">Sign up</router-link>
-        </li>
-      </ul>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav" v-if="isAuthenticated">
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">Wardrobe</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">My looks</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="d-inline-flex text-decoration-none" to="/my-profile">
+              <div class="avatar" :style="{backgroundImage: 'url(' + avatar + ')'}"></div>
+              <a class="username nav-link">{{ userName }}</a>
+            </router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav" v-else>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/registration">Sign up</router-link>
+          </li>
+        </ul>
 
-    </div>
-  </nav>
 
-  <Slide right class="d-md-none">
-    <a id="home" href="#">
-      <span>Home</span>
-    </a>
-  </Slide>
+      </div>
+    </nav>
+
 
   </div>
 </template>
 
 <script>
-import {Slide} from 'vue-burger-menu'
+import axios from 'axios'
 
 export default {
-  components: {Slide}
+  data() {
+    return {
+      isAuthenticated: false,
+      userName: null,
+      avatar: "https://upload.wikimedia.org/wikipedia/ru/thumb/9/9e/Lenin._Sculptors_Bogolyubov_and_Ingal._1950.png/240px-Lenin._Sculptors_Bogolyubov_and_Ingal._1950.png"
+    }
+  },
+
+  async mounted() {
+    const res = await axios.get('http://localhost:3000/me', {withCredentials: true})
+    if (res.data.name) {
+      this.isAuthenticated = true
+      this.userName = res.data.name
+      this.avatar = res.data.avatar || this.avatar
+    }
+  }
 }
 </script>
 
-<style>
-.bm-burger-button {
-  top: 4rem;
-  right: 1rem !important;
+<style scoped>
+.avatar {
+  margin-left: 1.5rem;
+
+  width: 40px;
+  height: 40px;
+  background-size: cover;
+  background-position: top center;
+  border-radius: 50%;
+}
+
+.username {
+  padding-left: 0.5rem;
+}
+
+.navbar-nav {
+  height: 2.5rem;
+  margin: 0.5rem 0 0.5rem 0;
 }
 </style>
